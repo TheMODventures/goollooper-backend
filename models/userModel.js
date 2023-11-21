f262c08799a26a97ec55ce1d250cd6a200c70af3
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2023-11-22 04:18:56 +0500

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -5,27 +5,67 @@ const { sign } = require('jsonwebtoken');
 const mongoosePaginate = require('mongoose-paginate-v2');
 const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
-const { ROLES, GENDERS } = require('../utils/constants');
+const { ROLES, GENDERS, LOCATIONS_TYPES } = require('../utils/constants');
 const { getMongooseAggregatePaginatedData } = require("../utils");
 
 const userSchema = new Schema({
     firstName: { type: String, default: null },
     lastName: { type: String, default: null },
+    userName: { type: String, default: null, unique: true, trim: true },
     email: { type: String, unique: true, required: true, lowercase: true, trim: true },
     password: { type: String, required: true, select: false },
-    dob: { type: Date, default: null },
     gender: { type: String, enum: Object.values(GENDERS), default: GENDERS.MALE },
     age: { type: Number, default: null },
     countryCode: { type: String },  // like 'PK' alpha-2 format
     phoneCode: { type: String },  // like '+92'
     phone: { type: String, default: null },
     completePhone: { type: String, select: false },
+    profileImage: { type: String },
+    gallery: [String],
+    about: { type: String, default: null },
+    role: { type: String, default: ROLES.USER, enum: Object.values(ROLES) },
+    volunteer: [{
+        service: { type: Schema.Types.ObjectId, ref: 'Service' },
+        subService: { type: Schema.Types.ObjectId, ref: 'SubService' },
+    }],
+    services: [{
+        service: { type: Schema.Types.ObjectId, ref: 'Service' },
+        subService: { type: Schema.Types.ObjectId, ref: 'SubService' },
+    }],
+    subscription: {
+        subscription: { type: Schema.Types.ObjectId, ref: 'Subscription' },
+        plan: { type: Schema.Types.ObjectId, ref: 'Plan' },
+    },
+    locationType: { type: String, default: LOCATIONS_TYPES.LOCAL, enum: Object.values(LOCATIONS_TYPES) },
     location: {
         type: { type: String, enum: ["Point"], default: "Point" },
         coordinates: { type: [Number, Number] },
     },
-    image: { type: String },
-    role: { type: String, default: 'user', enum: Object.values(ROLES) },
+    state: { type: String, default: null },
+    city: { type: String, default: null },
+    country: { type: String, default: null },
+    zipCode: [
+        {
+            code: { type: String, default: null },
+            isSelected: { type: Boolean, default: false },
+        }],
+    visuals: [String],
+    company: {
+        name: { type: String, default: null },
+        logo: { type: String, default: null },
+        website: { type: String, default: null },
+        affiliation: { type: String, default: null },
+        publication: { type: String, default: null },
+        resume: { type: String, default: null },
+    },
+    certificates: [String],
+    licenses: [String],
+    reference: {
+        name: { type: String, default: null },
+        contact: { type: String, default: null },
+    },
+    insurances: [String],
+    isProfileCompleted: { type: Boolean, default: false },
     isVerified: { type: Boolean, default: false },
     isActive: { type: Boolean, default: true },
     fcmToken: { type: String },
