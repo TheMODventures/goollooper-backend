0982d7e57ce3df28dc23b4799c3fa517e9d38739
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2024-01-26 13:04:45 +0500

    admin auth api completed

--- src/api/controllers/auth/auth.admin.controller.ts
+++ src/api/controllers/auth/auth.admin.controller.ts
@@ -13,6 +13,25 @@ class AuthController extends AuthBaseController {
     );
     return res.status(response.code).json(response);
   };
+
+  forgetPassword = async (req: Request, res: Response) => {
+    const { email } = req.body;
+    const response = await this.authService.forgotPassword(email);
+    return res.status(response.code).json(response);
+  };
+
+  resendOtp = async (req: Request, res: Response) => {
+    const { email } = req.body;
+    const response = await this.authService.resendOtp(email);
+    return res.status(response.code).json(response);
+  };
+
+  verifyOtp = async (req: Request, res: Response) => {
+    const userId = req?.locals?.auth?.userId!;
+    const { code } = req.body;
+    const response = await this.authService.verifyOtp(userId, code);
+    return res.status(response.code).json(response);
+  };
 }
 
 export default AuthController;
