2c8eb36425557427e79ffe5ddb3a4671b7a847ef
Mohammad Shahzaib <mohdshahzaib24@gmail.com>
2024-02-17 21:59:20 +0500

    created get chat api

--- src/api/controllers/chat/chat.controller.ts
+++ src/api/controllers/chat/chat.controller.ts
@@ -13,6 +13,19 @@ class ChatController {
     this.chatRepository = new ChatRepository();
   }
 
+  index = async (req: Request, res: Response) => {
+    const { limit, page, chatSupport = false, search } = req.query;
+    const limitNow = limit ? limit : 10;
+    const response = await this.chatService.getChats(
+      req.locals.auth?.userId as any,
+      Number(page),
+      Number(limitNow),
+      chatSupport as boolean,
+      search as string
+    );
+    return res.status(response.code).json(response);
+  };
+
   addRequest = async (req: Request, res: Response) => {
     const { id } = req.params;
     const dataset: Partial<IChat> = { ...req.body };
